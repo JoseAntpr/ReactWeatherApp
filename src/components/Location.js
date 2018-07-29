e9d283b9790b8825ab9a9c542b52c27281755994
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-07-29 15:33:31 +0200

    ADD: add props to WeatherExtraInfo & WeatherLocation

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
-const Location = () => (
-    <div><h1>Málaga</h1></div>
-);
+const Location = ({city}) => {
+    return (
+        <div>
+            <h1>{city}</h1>
+        </div>
+    );
+
+};
+    
+
 
 export default Location;
 
