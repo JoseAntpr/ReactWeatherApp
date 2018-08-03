384dd3ae19853a97763c6f77ab1df43f60bb9294
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-08-03 20:45:46 +0200

    ADD: add propTypes share and update the props of weather data

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,21 +1,27 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
 import './styles.css';
-import {
-    CLOUD,
-    CLOUDY,
-    SUN,
-    RAIN,
-    SNOW,
-    WINDY 
-} from '../../../constanst/weather';
 
-const WeatherData = () => (
-    <div className='weatherDataCont'>
-        <WeatherTemperature temperature={20} weatherState={SUN}/>
-        <WeatherExtraInfo humidity={80} wind={"10 m/s"}/>
-    </div>
-);
+
+const WeatherData = ({data}) => {
+    const { temperature, weatherState, humidity, wind } = data;
+    return (
+        <div className='weatherDataCont'>
+            <WeatherTemperature temperature={temperature} weatherState={weatherState}/>
+            <WeatherExtraInfo humidity={humidity} wind={wind}/>
+        </div>
+    );
+};
+
+WeatherData.propTypes = {
+    data: PropTypes.shape({
+        temperature: PropTypes.number.isRequired,
+        weatherState: PropTypes.string.isRequired,
+        humidity: PropTypes.number.isRequired,
+        wind: PropTypes.string.isRequired
+    })
+}
 
 export default WeatherData;
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
+import {
+    CLOUD,
+    CLOUDY,
+    SUN,
+    RAIN,
+    SNOW,
+    WINDY 
+} from '../../constanst/weather';
 import './styles.css';
 
+const data = {
+    temperature: 20,
+    weatherState: SUN,
+    humidity: 10,
+    wind: '10 m/s'
+}
 const WeatherLocation = () => (
     <div className='weatherLocationCont'>
         <Location city={'Málaga'} />
-        <WeatherData />
+        <WeatherData data={data}/>
     </div>
 );
 
